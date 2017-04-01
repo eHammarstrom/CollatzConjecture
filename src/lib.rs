15397a515c701de2040724184dccf4596aacd165
Emil Hammarstrom <emil.a.hammarstrom@gmail.com>
2017-04-01 19:47:09 +0200

    1:1 concurrency

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,7 @@
 use std::collections::HashSet;
+use std::collections::LinkedList;
+use std::thread;
+use std::sync::mpsc;
 
 /// Calculates the collatz conjecture for a given number `starting_number`.
 /// Returns a `Vec<u32>` of the number trail.
@@ -7,15 +10,15 @@ use std::collections::HashSet;
 /// ```
 /// let ls: Vec<u32> = collatzconjecture::number(10000);
 /// ```
-pub fn number(starting_number: u32) -> Vec<u32> {
-    let mut number_trail: Vec<u32> = Vec::new();
+pub fn number(starting_number: u32) -> LinkedList<u32> {
+    let mut number_trail: LinkedList<u32> = LinkedList::new();
 
     let mut temp = starting_number;
-    number_trail.push(temp);
+    number_trail.push_front(temp);
 
     while temp != 1 {
         temp = collatizer(temp);
-        number_trail.push(temp);
+        number_trail.push_front(temp);
     }
 
     number_trail
@@ -31,14 +34,26 @@ pub fn number(starting_number: u32) -> Vec<u32> {
 /// ```
 pub fn up_to(final_number: u32) -> HashSet<u32> {
     let mut hset: HashSet<u32> = HashSet::new();
+    let (tx, rx) = mpsc::channel();
 
     for i in 1..final_number {
-        let mut temp = i;
-        hset.insert(temp);
+        // TODO: Spawn threads which does the while calculation
+        // Collect these values and put them in the hashset synchronously
 
-        while temp != 1 {
-            temp = collatizer(temp);
-            hset.insert(temp);
+        let tx = tx.clone();
+        thread::spawn(move || {
+            // Send back number(i) vector for addition to hashset
+            tx.send(number(i))
+                .expect("Thread panicked when sending computation.");
+        });
+
+    }
+
+    drop(tx);
+
+    for recv in rx {
+        for i in recv {
+            hset.insert(i);
         }
     }
 
